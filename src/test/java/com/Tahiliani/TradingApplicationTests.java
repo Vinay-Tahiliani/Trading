99fcebad65f42d72e0f4bb2789f4bd3e0e0cb73e
Vinay-Tahiliani <vtahilia@kent.edu>
2024-06-27 17:13:15 -0400

    setup of the springboot application

---
+++ src/test/java/com/Tahiliani/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tahiliani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
